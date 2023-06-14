4bb2349a3069a86421903dcebabd4291282a2f9f
Carlos Moreno Serrano <carlosmoreno@meta.com>
2023-06-14 12:27:03 -0700

    Added GIPHY image search API

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 console.log("Hello")
 
+const api_key = "JSTUMgAWOn2NAFffrAamFQ85M6tlJiUm";
+
 var submit_button = document.getElementById("submit_button");
+var searchInput = document.getElementById("searchInput");
 
 /*
 submit_button.addEventListener("click", fetchGiphyImages);
@@ -13,6 +16,7 @@ function fetchGiphyImages(event) {
     console.log("Button clicked!");
 }
 
+
 (event) => {
     event.preventDefault(); //Prevent form submission (optional)
 
@@ -21,9 +25,32 @@ function fetchGiphyImages(event) {
 }
 */
 
-submit_button.addEventListener("click", (event) => {
+submit_button.addEventListener("click", async (event) => {
     event.preventDefault(); //Prevent form submission (optional)
 
     //Perform your desired action here
-    console.log("Button clicked!");
+    try {
+        // Perform the asynchronous action here
+        const searchTerm = searchInput.value;
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${encodeURIComponent(searchTerm)}`);
+        console.log(searchTerm);
+        const data = await response.json();
+
+
+        const gifs = data.data[0].images["original"].url;
+
+        data.data.forEach(gif => {
+            const img = document.createElement("img");
+            img.src = gif.images["original"].url;
+            gifsDiv.appendChild(img);
+        });
+
+        // Process the fetched data or perform other actions
+        console.log(gifs);
+    } catch (error) {
+        // Handle any errors that occur during the fetch
+        console.log("An error occurred:", error);
+    }
 });
+
+// https://api.giphy.com/v1/gifs/search?api_key=JSTUMgAWOn2NAFffrAamFQ85M6tlJiUm&q=Hello
